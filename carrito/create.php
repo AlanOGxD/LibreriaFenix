b0f60ffe5e73198ebb39dba7e484ee8dcdae1c3c
AlanOGxD <alanosvaldo88@gmail.com>
2020-12-12 19:59:56 -0600

    APIs carrito

---
+++ carrito/create.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once '../db/headers.php';
+include_once '../modelos/carrito.php';
+
+$carrito = new Carrito();
+
+// $data = json_decode(file_get_contents("php://input"));
+$data = (object)$_POST;
+
+if (
+    isset($data->isbn) &&
+    isset($data->usuario) &&
+    isset($data->fecha) &&
+    isset($data->cantidad)
+) {
+
+    $carrito->isbn = $data->isbn;
+    $carrito->usuario = $data->usuario;
+    $carrito->fecha = $data->fecha;
+    $carrito->cantidad = $data->cantidad;
+
+    $res = $carrito->AgregarACarrito();
+    if($res === TRUE){
+        http_response_code(201);
+        echo json_encode(array("success" => true, "status" => 201, "message" => "Registro creado con éxito.", "carrito" => $carrito));
+    } else {
+        http_response_code(503);
+        echo json_encode(array("success" => false, "status" => 503, "message" => $res));
+    }
+} else  {
+    http_response_code(400);
+    echo json_encode(array("success" => false, "status" => 400, "message" => "No se puede procesar la solicitud, los datos estan incompletos"));
+}
+
+
+?>
+
---
+++ carrito/readAll.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once '../db/headers.php';
+include_once '../modelos/carrito.php';
+
+$carrito = new Carrito();
+
+if(isset($_GET['usuario'])) {
+    $carrito->usuario = $_GET['usuario'];
+
+    $data = $carrito->GetCarritoPorUsuario();
+    if(count($data) > 0) {
+        http_response_code(200);
+        echo json_encode(array("success" => true, "status" => 200, "carrito" => $data));
+    } else {
+        http_response_code(503);
+        echo json_encode(array("success" => false, "status" => 503, "carrito" => $data));
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(array("success" => false, "status" => 400, "message" => "No se puede procesar la solicitud, los datos estan incompletos"));
+}
+
+?>
