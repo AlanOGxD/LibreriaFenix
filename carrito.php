efc2068c2f8d9e12473af8af44cd251a2e35506f
AlanOGxD <alanosvaldo88@gmail.com>
2020-12-12 22:29:47 -0600

    Create carrito.php

---
+++ carrito.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/icofont/icofont.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="w-100 bg-red">
+
+                <?php include_once('common/menu.php') ?>
+
+            </div>
+        </div>
+
+        <p>&nbsp;</p>
+
+        <div class="container text-center">
+            <div class="row">
+                <h3 class="title">Carrito de compra</h3>
+                <div class="col-12 col-sm-8">
+                    <div class="card text-left">
+                        <img class="card-img-top" src="holder.js/100px180/" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title"> <strong>En el carrito (2 elementos)</strong> </h5>
+
+                            <p class="card-text">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-12 col-sm-3">
+                                            <img class="card-img" src="http://magento2.flytheme.net/themes/sm_amabook/pub/media/catalog/product/cache/f6fa453a447ca6aa5d835f6b9382404a/2/_/2_2_1.jpg">
+                                        </div>
+                                        <div class="col-12 col-sm-9">
+                                            <div class="container">
+                                                <div class="row p-2">
+                                                    <div class="col-12 col-sm-6">
+                                                        <p> <strong> A Farewell to Arms </strong> </p>
+                                                        <p class="card-subtitle">Adam Nayman</p>
+                                                    </div>
+                                                    <div class="col-12 col-sm-6 text-center">
+                                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                                            <button type="button" class="btn btn-light">-</button>
+                                                            <input class="form-control  text-center" value="1">
+                                                            <button type="button" class="btn btn-light">+</button>
+                                                        </div>
+                                                        <div class="card-text text-danger">
+                                                            <p><strong>MXN $150,00</strong></p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        After disappearing for three years, Artemis Fowl has returned to a life different from the one he left.
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col p-2">
+                                                        <button class="btn btn-outline-primary btn-sm capitalize">
+                                                            <i class="icofont-shopping-cart"></i> Añadir al carrito
+                                                        </button>
+                                                        <button class="btn btn-outline-danger btn-sm capitalize">
+                                                            <i class="icofont-ui-delete"></i> Remover
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </p>
+
+                            <p class="card-text">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-12 col-sm-3">
+                                            <img class="card-img" src="http://magento2.flytheme.net/themes/sm_amabook/pub/media/catalog/product/cache/f6fa453a447ca6aa5d835f6b9382404a/9/_/9_3_1.jpg">
+                                        </div>
+                                        <div class="col-12 col-sm-9">
+                                            <div class="container">
+                                                <div class="row p-2">
+                                                    <div class="col-12 col-sm-6">
+                                                        <p> <strong> A Farewell to Arms </strong> </p>
+                                                        <p class="card-subtitle">Adam Nayman</p>
+                                                    </div>
+                                                    <div class="col-12 col-sm-6 text-center">
+                                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                                            <button type="button" class="btn btn-light">-</button>
+                                                            <input class="form-control  text-center" value="1">
+                                                            <button type="button" class="btn btn-light">+</button>
+                                                        </div>
+                                                        <div class="card-text text-danger">
+                                                            <p><strong>MXN $150,00</strong></p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        After disappearing for three years, Artemis Fowl has returned to a life different from the one he left.
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col p-2">
+                                                        <button class="btn btn-outline-primary btn-sm capitalize">
+                                                            <i class="icofont-shopping-cart"></i> Añadir al carrito
+                                                        </button>
+                                                        <button class="btn btn-outline-danger btn-sm capitalize">
+                                                            <i class="icofont-ui-delete"></i> Remover
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-4">
+                    <div class="card text-left">
+                        <img class="card-img-top" src="holder.js/100px180/" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">  <strong>Total a pagar</strong> </h5>
+                            <p class="card-text">
+                                <table class="table table-light">
+                                    <tbody >
+                                        <tr>
+                                            <td> Subtotal: </td>
+                                            <td> MXN $300,00 </td>
+                                        </tr>
+                                        <tr>
+                                            <td> Envios: </td>
+                                            <td> Gratis </td>
+                                        </tr>
+                                        <tr class="border-top">
+                                            <td> Total: </td>
+                                            <td> MXN $300,00 </td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2">
+                                                <button class="btn btn-primary btn-block btn-lg capitalize">Pagar</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+
+    <?php include_once('common/footer.php') ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
